723696d75ee28cee4836305f3fa2ad302424aec2
Eduardo Erick <90888167+whoiserick@users.noreply.github.com>
2023-09-24 23:21:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
-import requests  # Adicione esta linha no início do seu código
+import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
@@ -11,23 +12,32 @@ def criar_diretorio(nome_usuario):
     if not os.path.exists(nome_usuario):
         os.makedirs(nome_usuario)
 
-# Caminho para o ChromeDriver na pasta raiz do projeto
-chromedriver_path = os.path.join(os.getcwd(), 'chromedriver')
-
 # Inicialize o WebDriver do Chrome
-driver = webdriver.Chrome(chromedriver_path)
+driver = webdriver.Chrome()
 
-# URL do perfil do Twitter
-url = 'https://twitter.com/ukazuhira'
+# URL do login do Twitter
+url_login = 'https://twitter.com/login'
 
-# Obtenha o nome de usuário a partir da URL
-nome_usuario = url.split('/')[-1]
+# Credenciais da sua conta
+usuario = 'seu_usuario'
+senha = 'sua_senha'
 
-# Abra a página no navegador
-driver.get(url)
+# Abra a página de login do Twitter
+driver.get(url_login)
+
+# Aguarde até que os campos de login e senha estejam presentes na página
+wait = WebDriverWait(driver, 10)
+elemento_usuario = wait.until(EC.presence_of_element_located((By.NAME, 'session[username_or_email]')))
+elemento_senha = wait.until(EC.presence_of_element_located((By.NAME, 'session[password]')))
+
+# Preencha os campos de login e senha
+elemento_usuario.send_keys(usuario)
+elemento_senha.send_keys(senha)
+
+# Envie o formulário de login
+elemento_senha.send_keys(Keys.RETURN)
 
 # Aguarde até que todas as imagens estejam carregadas
-wait = WebDriverWait(driver, 10)
 elementos_imagens = wait.until(EC.presence_of_all_elements_located((By.TAG_NAME, 'img')))
 
 # Crie uma instância do BeautifulSoup com o conteúdo atualizado da página
@@ -35,6 +45,9 @@ soup = BeautifulSoup(driver.page_source, 'html.parser')
 
 # Encontre as imagens na página
 imagens = soup.find_all('img')
+
+# Obtenha o nome de usuário a partir da URL
+nome_usuario = usuario
 
 # Chame a função para criar o diretório
 criar_diretorio(nome_usuario)
