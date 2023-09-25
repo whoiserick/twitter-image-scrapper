86cb7a155b49677b4d9147543161a0d36717d1a5
Eduardo Erick <90888167+whoiserick@users.noreply.github.com>
2023-09-24 22:28:21 -0300

    att

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -7,12 +8,19 @@ from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 
+# Função para criar diretório se não existir
+def criar_diretorio(nome_usuario):
+    if not os.path.exists(nome_usuario):
+        os.makedirs(nome_usuario)
+
 # Inicialize o WebDriver do Chrome
 driver = webdriver.Chrome(ChromeDriverManager().install())
-# driver = webdriver.Chrome('./chromedriver.exe')
 
 # URL do perfil do Twitter
 url = 'https://twitter.com/ukazuhira'
+
+# Obtenha o nome de usuário a partir da URL
+nome_usuario = url.split('/')[-1]
 
 # Abra a página no navegador
 driver.get(url)
@@ -27,11 +35,14 @@ soup = BeautifulSoup(driver.page_source, 'html.parser')
 # Encontre as imagens na página
 imagens = soup.find_all('img')
 
+# Chame a função para criar o diretório
+criar_diretorio(nome_usuario)
+
 # Itera sobre as imagens e faça o download
 for i, img in enumerate(imagens):
     url = img['src']
     response = requests.get(url)
-    with open(f'imagem_{i}.jpg', 'wb') as f:
+    with open(f'{nome_usuario}/imagem_{i}.jpg', 'wb') as f:
         f.write(response.content)
 
 # Feche o navegador
