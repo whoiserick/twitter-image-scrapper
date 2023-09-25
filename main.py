8aa1fdea93b3532ed3bfbb9745fb29e0e874335e
Eduardo Erick <90888167+whoiserick@users.noreply.github.com>
2023-09-25 00:14:55 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,8 @@ import os
 import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from tqdm import tqdm
 
 # Função para criar diretório se não existir
 def criar_diretorio(nome_usuario):
@@ -21,7 +19,7 @@ url_perfil = 'https://twitter.com/ukazuhira'
 # Abra a página do perfil do Twitter
 driver.get(url_perfil)
 
-# Aguarde até que todas as imagens estejam presentes, com um tempo de espera de 20 segundos
+# Aguarde até que todas as imagens estejam presentes
 wait = WebDriverWait(driver, 20)
 elementos_imagens = wait.until(EC.presence_of_all_elements_located((By.XPATH, '//img[@srcset]')))
 
@@ -32,17 +30,15 @@ nome_usuario = url_perfil.split('/')[-1]
 criar_diretorio(nome_usuario)
 
 # Encontre todas as imagens na página
-imagens = driver.find_elements(By.TAG_NAME, 'img')
-
-# Filtrar apenas as imagens com o atributo src definido
-imagens_com_src = [img for img in imagens if img.get_attribute('src')]
-
+imagens = driver.find_elements(By.XPATH, '//img[@srcset]')
 
 # Itera sobre as imagens e faça o download
-for i, img in tqdm(enumerate(imagens_com_src), total=len(imagens_com_src), desc='Baixando imagens'):
+for i, img in enumerate(imagens):
     url = img.get_attribute('src')
+    identificador = url.split('/')[-1].split('?')[0]
+    extensao = url.split('=')[-1]
     response = requests.get(url)
-    with open(f'{nome_usuario}/imagem_{i}.jpg', 'wb') as f:
+    with open(f'{nome_usuario}/imagem_{i}.{extensao}', 'wb') as f:
         f.write(response.content)
 
 # Feche o navegador
